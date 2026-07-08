0eedf7493e5b1de9ec041cac0cd91d26ec4b236b
Subhadeep Paul <subhapaul637@gmail.com>
2026-07-08 20:49:19 +0530

    Time: 0 ms (100%), Space: 58.3 MB (27.3%) - LeetHub

--- 0704-binary-search/0704-binary-search.js
+++ 0704-binary-search/0704-binary-search.js
@@ -4,19 +4,20 @@
  * @return {number}
  */
 var search = function(nums, target) {
-    let low = 0;
-    let high = nums.length-1;
+    let low =0;
+    let high = nums.length;
     while(low<=high){
         let guess = Math.floor((low+high)/2);
         if(nums[guess] === target){
             return guess;
         }
-        else if(nums[guess]<target){
-            low = guess + 1;
+        else if(nums[guess] < target){
+            low = guess+1;
         }
         else{
-            high = guess - 1;
+            high = guess -1;
         }
     }
-    return -1;
+    return -1
+
 };
