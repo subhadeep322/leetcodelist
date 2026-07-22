c83ffb572edde481b05c27bef4b873c34e3997bd
Subhadeep Paul <subhapaul637@gmail.com>
2026-07-22 20:59:04 +0530

    Time: 11 ms (98.19%), Space: 80.1 MB (31.27%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.js
+++ 0739-daily-temperatures/0739-daily-temperatures.js
@@ -3,15 +3,18 @@
  * @return {number[]}
  */
 var dailyTemperatures = function(temperatures) {
-    const n = temperatures.length;
-    const answer = new Array(n).fill(0);
-    const stack =[];
-    for(let i =0;i<n;i++){
-        while(stack.length >0 && temperatures[i]>temperatures[stack[stack.length-1]]){
-            const previdx = stack.pop();
-            answer[previdx] = i -previdx;
-        }
-        stack.push(i);
+  const n = temperatures.length;
+  let ans = new Array(n).fill(0);
+  let stack = [];
+  for(let i=n-1;i>=0;i--){
+    while(stack.length > 0 && temperatures[stack[stack.length-1]] <= temperatures[i]){
+        stack.pop();
     }
-    return answer;
+    if(stack.length > 0){
+        let nextwarmday = stack[stack.length -1];
+        ans[i]  = nextwarmday - i;
+    }
+    stack.push(i)
+  }
+  return ans;
 };
