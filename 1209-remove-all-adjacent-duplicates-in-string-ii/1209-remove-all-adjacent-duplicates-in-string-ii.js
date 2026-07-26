17ca9080d8b578c6bf7513660ecd7c04277b1dd0
Subhadeep Paul <subhapaul637@gmail.com>
2026-07-26 23:47:00 +0530

    Time: 30 ms (22.91%), Space: 65.7 MB (34.36%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.js
@@ -10,31 +10,24 @@ var removeDuplicates = function(s, k) {
     for(let i = 0; i < n; i++) {
         let c = s[i];
         
-        // 1. If stack is empty
         if(stack.length === 0) {
             stack.push({ char: c, count: 1 });
             continue;
         }
         
-        // 2. If the top character does NOT match the current character
         if(stack[stack.length - 1].char !== c) {
             stack.push({ char: c, count: 1 });
             continue;
         }
         
-        // 3. If the top character MATCHES the current character
         if(stack[stack.length - 1].count < k - 1) {
-            // Count is less than k-1, so we pop, add 1, and push back
             let p = stack.pop();
             stack.push({ char: c, count: p.count + 1 });
         } else {
-            // Count has reached k-1, adding this one makes it k.
-            // So we delete them by just popping!
             stack.pop();
         }
     }
     
-    // 4. Build the result string from the stack
     let res = "";
     while (stack.length > 0) {
         let p = stack.pop();
@@ -46,6 +39,5 @@ var removeDuplicates = function(s, k) {
         }
     }
 
-    // 5. Reverse the string because stack pops bottom-up (LIFO)
     return res.split('').reverse().join('');
 };
