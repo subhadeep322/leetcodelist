a1ddd7094649b9777ca0bd024edbb6241216e25d
Subhadeep Paul <subhapaul637@gmail.com>
2026-09-17 20:27:01 +0530

    Time: 37 ms (50.65%), Space: 73.4 MB (16.42%) - LeetHub

--- 0015-3sum/0015-3sum.js
+++ 0015-3sum/0015-3sum.js
@@ -2,40 +2,39 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var threeSum = function(nums) {
-     const res = [];
-    nums.sort((a, b) => a - b);
-    for (let i = 0; i < nums.length - 2; i++) {
-        if (i > 0 && nums[i] === nums[i - 1]) {
-            continue;
-        }
-        let left = i + 1;
-        let right = nums.length - 1;
-        const tar = -nums[i];
+var threeSum = function (nums) {
+let res = [];
+nums.sort((a,b)=>a-b);
 
-        while(left<right){
-            const sum = nums[left]+nums[right]
-            if(sum === tar){
-                res.push([nums[i],nums[left],nums[right]])
-                left++;
-                right--;
-
-                while(left<right && nums[left] === nums[left-1]){
-                    left++
-                }
-                while(left<right && nums[right] === nums[right+1]){
-                    right--;
-                }
-            }
-            else if(sum<tar){
-                left++;
-            }
-            else{
-                right--;
-            }
-
-        }
-
+for(let i=0;i<nums.length-2;i++){
+    if( i>0 &&nums[i] === nums[i-1]){
+        continue;
     }
-    return res;
+let left = i+1;
+let right = nums.length-1;
+let tar = -nums[i];
+let sum = 0;
+while(left<right){
+    sum = nums[left]+nums[right];
+    if(sum === tar){
+        res.push([nums[i],nums[left],nums[right]]);
+        left++;
+        right--;
+    
+    while(left<right && nums[left] === nums[left-1]){
+        left++;
+    }
+    while(left<right && nums[right] === nums[right+1]){
+        right--;
+    }
+    }
+   else if(sum<tar){
+    left++;
+   }else{
+    right--;
+   }
+}
+
+}
+return res;
 };
